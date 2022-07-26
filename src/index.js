7505b90109e170591bd886fe51c020477a1df187
Yury Maruk <maruk.dev@gmail.com>
2022-07-26 19:22:25 +0300

    feat: test is passing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined || matrix.length === 0 ) {
+    return [];
+  } else {
+    return matrix.map( (item, i) => i % 2 !== 0 ? item.reverse() : item).flat(Infinity);
+  }
+};
